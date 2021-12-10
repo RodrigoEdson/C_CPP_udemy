57741af512a68d1a10ebaa92f7976355f56946a4
Rodrigo Edson Fernandes <rodrigoedson@gmail.com>
2021-12-10 08:05:20 -0300

    Binary search and functions get/set/max/min/avg

--- arrayADT/firstArrayADT.cpp
+++ arrayADT/firstArrayADT.cpp
@@ -97,6 +97,118 @@ int linearSearch(struct Array *arr, int key)
     return -1;
 }
 
+int binarySearch(struct Array arr, int key)
+{
+    int lowIndex = 0, highIndex = arr.length - 1, middle;
+
+    while (lowIndex <= highIndex)
+    {
+        middle = (lowIndex + highIndex) / 2;
+        printf("%d ", middle);
+        if (arr.A[middle] == key)
+            return middle;
+        else if (key > middle)
+            lowIndex = middle + 1;
+        else
+            highIndex = middle - 1;
+    }
+
+    return -1;
+}
+
+void sortArray(struct Array *arr)
+{
+    int highestIndexNotOrdered = arr->length;
+    int temp, i;
+    bool changed = true;
+
+    while (changed)
+    {
+        i = 0;
+        changed = false;
+        while (i < highestIndexNotOrdered)
+        {
+            if (arr->A[i] > arr->A[i + 1])
+            {
+                swapEements(&arr->A[i], &arr->A[i + 1]);
+                changed = true;
+            }
+            i++;
+        }
+        highestIndexNotOrdered--;
+    }
+}
+
+int binarySearchRecursive(struct Array arr, int lowIndex, int highIndex, int key)
+{
+    int middle;
+
+    if (lowIndex <= highIndex)
+    {
+        middle = (lowIndex + highIndex) / 2;
+        printf("%d ", middle);
+        if (arr.A[middle] == key)
+        {
+            return middle;
+        }
+        else if (key > middle)
+            return binarySearchRecursive(arr, middle + 1, highIndex, key);
+        else
+            return binarySearchRecursive(arr, lowIndex, middle - 1, key);
+    }
+
+    return -1;
+}
+
+int get(struct Array arr, int index)
+{
+    if (index >= 0 && index < arr.length)
+        return arr.A[index];
+    else
+        return -1;
+}
+
+void set(struct Array *arr, int index, int value)
+{
+    if (index >= 0 && index < arr->length)
+        arr->A[index] = value;
+}
+
+int max(struct Array arr)
+{
+    int m = arr.A[0];
+    for (int i = 0; i < arr.length; i++)
+    {
+        if (m < arr.A[i])
+            m = arr.A[i];
+    }
+    return m;
+}
+
+int min(struct Array arr)
+{
+    int m = arr.A[0];
+    for (int i = 0; i < arr.length; i++)
+    {
+        if (m > arr.A[i])
+            m = arr.A[i];
+    }
+    return m;
+}
+
+int sum(struct Array arr)
+{
+    int s;
+    for (int i = 0; i < arr.length; i++)
+        s += arr.A[i];
+    return s;
+}
+
+float avg(struct Array arr)
+{
+    return (float) sum(arr) / arr.length;
+}
+
 int main(int argc, char const *argv[])
 {
 
@@ -120,11 +232,29 @@ int main(int argc, char const *argv[])
     printf("After delete:\n");
     printArray(arr);
 
-    printf("Key 12 is in %d position\n", linearSearch(&arr, 12));
-    printf("Key 12 is in %d position\n", linearSearch(&arr, 12));
-    printf("Key 12 is in %d position\n", linearSearch(&arr, 12));
+    printf("Key 12 is in %d position + (Swap)\n", linearSearch(&arr, 12));
+    printf("Key 12 is in %d position + (Swap)\n", linearSearch(&arr, 12));
+    printf("Key 12 is in %d position + (Swap)\n", linearSearch(&arr, 12));
     printf("After Search:\n");
     printArray(arr);
+
+    // Need sort Array to use binary search
+    sortArray(&arr);
+    printf("After sort:\n");
+    printArray(arr);
+
+    printf("Bin - key 12 is in the %d position\n", binarySearch(arr, 12));
+    printf("BinRec - key 12 is in the %d position\n", binarySearch(arr, 12));
+
+    printf("Get 2 Element: %d \n", get(arr,2));
+    printf("Max value = %d \n", max(arr));
+    printf("Min value = %d \n", min(arr));
+    printf("Sum of values = %d \n", sum(arr));
+    printf("Avg value = %f \n", avg(arr));
+    printf("Set 2 Element to 10\n");
+    set(&arr,2,10);
+    printArray(arr);
+
 
     return 0;
 }
